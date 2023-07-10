bac6e0c4d3a889641025a8e6544f1f56a4036047
mohanapriya0711 <139154419+mohanapriya0711@users.noreply.github.com>
2023-07-10 23:35:57 +0530

    Add files via upload

---
+++ Reversing a tuple.py
@@ -0,0 +1,6 @@
+def reverse_tuple(t):
+    reversed_tuple = t[::-1]
+    return reversed_tuple
+tuples = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h')
+reversed_tuples = reverse_tuple(tuples)
+print(reversed_tuples)
